cdfe4abaab188654480f544951d68afcd030e9fb
Kevin <magavakevin10@gmail.com>
2022-08-29 13:57:19 -0400

    commit tree ef14b071173dff4fc1b0d9a81eea339941a08632

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+def greet_programmer 
+    print("Hello, programmer!")
+end
+
+def greet(name)
+    print("Hello, #{name}!")
+end
+
+def greet_with_default (name = "programmer")
+    print("Hello, #{name}!")
+
+end
+
+def add(num1, num2) 
+    return num1 + num2
+end
+
+def halve(num) 
+    if num.class != Integer
+        return nil
+    end
+    return num/2
+end
+
